cf671dc68090506782f2b3a337a7bfc11c21e37c
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-03-02 03:39:34 +0300

    feat: did the Add and Contains method

---
+++ homework_07_03_2025/BorDataStructure/BorDataStructure/Bor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace BorDataStructure;
+
+public class Bor
+{
+    public int Size { get; } = 0;
+    private Node root = new();
+
+    private class Node
+    {
+        public bool EndOfWord { get; set; } = false;
+        public Dictionary<char, Node> Nodes { get; set; } = new();
+    }
+
+    public bool Add(string str)
+    {
+        var missingLine = false;
+        var len = str.Length;
+        var node = this.root;
+        for (var i = 0; i < len; i++)
+        {
+            if (!node.Nodes.ContainsKey(str[i]))
+            {
+                missingLine = true;
+                node.Nodes[str[i]] = new Node();
+                node.Nodes[str[i]].EndOfWord = i == len - 1;
+            }
+            else if (!node.Nodes[str[i]].EndOfWord && i == len - 1)
+            {
+                missingLine = true;
+                node.Nodes[str[i]].EndOfWord = true;
+            }
+            node = node.Nodes[str[i]];
+        }
+        return missingLine;
+    }
+
+    public bool Contains(string str)
+    {
+        var len = str.Length;
+        var node = this.root;
+        for (var i = 0; i < len; i++)
+        {
+            if (!node.Nodes.ContainsKey(str[i]) || (i == len - 1 && !node.Nodes[str[i]].EndOfWord))
+            {
+                return false;
+            }
+            node = node.Nodes[str[i]];
+        }
+        return true;
+    }
+}
---
+++ homework_07_03_2025/BorDataStructure/BorDataStructure/Program.cs
@@ -0,0 +1,8 @@
+﻿using BorDataStructure;
+
+var qwe = new Bor();
+
+Console.WriteLine($"{qwe.Add("qwer")}");
+Console.WriteLine($"{qwe.Add("qwe")}");
+Console.WriteLine($"{qwe.Add("qwe")}");
+Console.WriteLine($"{qwe.Contains("qwe")}");
