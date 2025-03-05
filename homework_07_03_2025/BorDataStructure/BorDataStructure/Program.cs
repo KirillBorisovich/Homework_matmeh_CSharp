873f3814a275c6b08169de57f29d3c3a662a6a69
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-03-05 20:28:31 +0300

    refactor: added tests

--- homework_07_03_2025/BorDataStructure/BorDataStructure/Program.cs
+++ homework_07_03_2025/BorDataStructure/BorDataStructure/Program.cs
@@ -1,9 +1,3 @@
 ﻿using BorDataStructure;
 
-var qwe = new Bor();
-
-Console.WriteLine($"{qwe.Add("qwer")}");
-Console.WriteLine($"{qwe.Add("qwe")}");
-Console.WriteLine($"{qwe.Add("qwe")}");
-Console.WriteLine($"{qwe.Contains("qwe")}");
-Console.WriteLine($"{qwe.HowManyStartsWithPrefix("qw")}");
+var bor = new Bor();
