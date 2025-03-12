48c9f94eb5bed2f9c4c80ff67b4e66083192023d
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-03-12 16:57:03 +0300

    refactor: Forgot to remove comments and added a file extension check for decompression

--- homework_14_03_25/LZW/LZW/Program.cs
+++ homework_14_03_25/LZW/LZW/Program.cs
@@ -1,26 +1,30 @@
 ﻿using LZW;
 
-//string path = args[0];
-//string keyEvents = args[0];
+string path = args[0];
+string keyEvents = args[0];
 
-//if (!File.Exists(path))
-//{
-//    Console.WriteLine("The file does not exist\n");
-//    return -1;
-//}
+if (!File.Exists(path))
+{
+    Console.WriteLine("The file does not exist\n");
+    return -1;
+}
 
-//switch (keyEvents)
-//{
-//    case "-c":
-//        LZWTransform.Compress(path);
-//        break;
-//    case "-u":
-//        LZWTransform.Uncompress(path);
-//        break;
-//}
+switch (keyEvents)
+{
+    case "-c":
+        LZWTransform.Compress(path);
+        break;
+    case "-u":
+        if (path.Substring(path.Length - 7) != ".zipped")
+        {
+            Console.WriteLine("Incorrect extension for uncopression\n");
+            return -1;
+        }
 
-//Console.WriteLine("All good!\n");
+        LZWTransform.Uncompress(path);
+        break;
+}
 
-LZWTransform.Compress("C:\\Users\\Kiril\\OneDrive\\Рабочий стол\\text.txt");
+Console.WriteLine("All good!\n");
 
 return 0;
