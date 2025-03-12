c23aa14a8b0535ccf70193d76b733a0810745b21
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-03-12 12:49:36 +0300

    feat: add manage by console

--- homework_14_03_25/LZW/LZW/Program.cs
+++ homework_14_03_25/LZW/LZW/Program.cs
@@ -1,3 +1,26 @@
 ﻿using LZW;
 
-Console.WriteLine("LZW compression algorithm\n");
+//string path = args[0];
+//string keyEvents = args[0];
+
+//if (!File.Exists(path))
+//{
+//    Console.WriteLine("The file does not exist\n");
+//    return -1;
+//}
+
+//switch (keyEvents)
+//{
+//    case "-c":
+//        LZWTransform.Compress(path);
+//        break;
+//    case "-u":
+//        LZWTransform.Uncompress(path);
+//        break;
+//}
+
+//Console.WriteLine("All good!\n");
+
+LZWTransform.Compress("C:\\Users\\Kiril\\OneDrive\\Рабочий стол\\text.txt");
+
+return 0;
