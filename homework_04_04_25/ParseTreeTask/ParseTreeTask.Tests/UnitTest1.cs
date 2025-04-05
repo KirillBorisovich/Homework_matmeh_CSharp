e7458792d9b69390f401f04d66bd0853900724c7
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-04-05 13:33:17 +0300

    start: started doing task

---
+++ homework_04_04_25/ParseTreeTask/ParseTreeTask.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace ParseTreeTask.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
