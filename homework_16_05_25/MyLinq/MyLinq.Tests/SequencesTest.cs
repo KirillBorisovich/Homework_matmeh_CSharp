cf79bd351a4ae28972b1f1c7ff4c01c7cf77cfe5
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2025-05-14 16:02:59 +0300

    feat: did the functional part

---
+++ homework_16_05_25/MyLinq/MyLinq.Tests/SequencesTest.cs
@@ -0,0 +1,83 @@
+﻿namespace MyLinq.Tests;
+
+public class SequencesTest
+{
+    private int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GetPrimesTest()
+    {
+        var primeNumbers = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+        var counter = 0;
+        foreach (var item in Sequences.GetPrimes())
+        {
+            if (counter == primeNumbers.Length)
+            {
+                break;
+            }
+
+            if (item != primeNumbers[counter])
+            {
+                Assert.Fail();
+            }
+
+            counter++;
+        }
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TakeTest()
+    {
+        var elementNumber = 3;
+        var counter = 0;
+        foreach (var item in Sequences.Take(this.array, elementNumber))
+        {
+            if (item != this.array[counter] || counter > elementNumber)
+            {
+                Assert.Fail();
+            }
+
+            counter++;
+        }
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TakeNullReferenceExceptionTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            foreach (var item in Sequences.Take(this.array, 6))
+            {
+                var i = item;
+            }
+        });
+    }
+
+    [Test]
+    public void SKipTest()
+    {
+        var elementNumber = 2;
+        var counter = 0;
+        foreach (var item in Sequences.Skip(this.array, elementNumber))
+        {
+            if (item != this.array[counter + 1] ||
+                counter > this.array.Length - elementNumber)
+            {
+                Assert.Fail();
+            }
+
+            counter++;
+        }
+
+        Assert.Pass();
+    }
+}
---
+++ homework_16_05_25/MyLinq/MyLinq/Sequences.cs
@@ -0,0 +1,119 @@
+﻿// <copyright file="Sequences.cs" company="BengyaKirill">
+// Copyright (c) BengyaKirill under MIT License. All rights reserved.
+// </copyright>
+
+namespace MyLinq;
+
+/// <summary>
+/// Class for slave with sequences.
+/// </summary>
+public static class Sequences
+{
+    /// <summary>
+    /// Get a sequence of prime numbers.
+    /// </summary>
+    /// <returns>Sequence of prime numbers.</returns>
+    public static IEnumerable<int> GetPrimes()
+    {
+        int number = 2;
+        while (true)
+        {
+            if (IsPrime(number))
+            {
+                yield return number;
+            }
+
+            number++;
+        }
+
+        static bool IsPrime(int number)
+        {
+            int counter = 0;
+            for (var divider = 1; divider <= Math.Sqrt(number); divider++)
+            {
+                if (number % divider == 0)
+                {
+                    counter++;
+                    if (divider != number / divider)
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter >= 3)
+                {
+                    return false;
+                }
+            }
+
+            return counter == 2;
+        }
+    }
+
+    /// <summary>
+    /// Take a sequence of the first n elements of a sequence.
+    /// </summary>
+    /// <typeparam name="T">Parameter type of sequence elements.</typeparam>
+    /// <param name="seq">A sequence from which a
+    /// subsequence must be returned.</param>
+    /// <param name="n">The number of the element
+    /// to which you need to return.</param>
+    /// <returns>Subsequences of a sequence.</returns>
+    /// <exception cref="NullReferenceException">Zero sequence
+    /// transmitted.</exception>
+    /// <exception cref="FormatException">Invalid parameter passed.</exception>
+    public static IEnumerable<T> Take<T>(this IEnumerable<T> seq, int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var counter = 0;
+        var enumerator = seq.GetEnumerator();
+        do
+        {
+            if (!enumerator.MoveNext() && counter < n)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            yield return enumerator.Current;
+            counter++;
+        }
+        while (counter < n);
+    }
+
+    /// <summary>
+    /// Get sequence without first n elements.
+    /// </summary>
+    /// <typeparam name="T">Parameter type of sequence elements.</typeparam>
+    /// <param name="seq">A sequence from which a
+    /// subsequence must be returned.</param>
+    /// <param name="n">The number of the element to skip to</param>
+    /// <returns>Subsequences of a sequence.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Invalid parameter passed.</exception>
+    public static IEnumerable<T> Skip<T>(this IEnumerable<T> seq, int n)
+    {
+        var counter = 0;
+        var enumerator = seq.GetEnumerator();
+        do
+        {
+            if (!enumerator.MoveNext() && counter < n)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (counter < n)
+            {
+                counter++;
+                continue;
+            }
+
+            yield return enumerator.Current;
+            counter++;
+        }
+        while (counter < n);
+    }
+}
